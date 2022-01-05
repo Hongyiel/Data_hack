0ee91a41581bfab88203f01fc8b0b36eacb89a5b
Dongwoo Kang <kangdo@oregonstate.edu>
2022-01-04 22:28:44 -0800

    flask rendering template

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug = True, port=5001)
+
+response = requests.get("https://api.covidactnow.org/v2/states.json?apiKey=8d65e6de718b4c6da48d818e9a204909")
+print(response.text)
