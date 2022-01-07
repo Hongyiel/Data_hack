d93baba494393bd2e3cbc6e2de1a6b0c7f4afe61
yyoongs <jysid3531@gmail.com>
2022-01-07 03:09:19 -0800

    Separate index(map) page and home(default) page

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("index.html", response_info=response_info)
+    return render_template("home.html")
 
+@app.route('/index')
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
