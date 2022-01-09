65ef182067e4e7e2b8f8bc47030cb2cf2ac0679f
Hongyiel Suh <55763367+Hongyiel@users.noreply.github.com>
2022-01-08 23:33:39 -0800

    Data setting
    init

--- get_data.py
+++ get_data.py
@@ -9,14 +9,28 @@ def _data(response_info):
     print("State in " + response_info[0]['country'])
 
     for i in range(len(response_info)):
-        print(i)
-        print("State: " + response_info[i]['state'])
-        print(response_info[i]['state'])
+        # print(i)
+        # print("State: " + response_info[i]['state'])
+        # print(response_info[i]['state'])
         state_data.append(response_info[i]['state'])
-
-
         # data[i] = response_info[i]['population']
         # actual_data(response_info, i)
-
-    print(state_data)
     return state_data
+
+population_data = []
+# population data
+def pop_data(response_info):
+    for i in range(len(response_info)):
+        population_data.append(response_info[i]['population'])
+
+    return population_data
+
+
+# set data numbers )Ex. if 'OR' then return 38 numbers for data
+def all_data(state,pop):
+    return [state, pop]
+
+def sorting_data(name,state_data,real_data):
+    for i in range(len(state_data)):
+        if name == state_data[i]:
+            return real_data[i]
